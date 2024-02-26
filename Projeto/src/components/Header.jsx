7209016f4fe3f73fd5d0d75a7da9fbb6fb0c6aab
Pedro <vegless2001@gmail.com>
2024-02-26 21:23:14 +0000

    menu

--- Projeto/src/components/Header.jsx
+++ Projeto/src/components/Header.jsx
@@ -2,11 +2,18 @@ import Button_Route from "./Button_Route";
 import Switch from "./Switch";
 import Data from "./Data";
 import Temp from "./Temp";
+import {CiMenuFries} from "react-icons/ci"
+import { useState } from "react";
 
 /*
     Um componente de cabeçalho que contém elementos comuns de navegação e informações.
  */
 function Header() {
+    const [estaAberto, setEstaAberto] = useState(false);
+
+  const toggleAberto = () => {
+    setEstaAberto(!estaAberto);
+  };
     // Renderiza o componente do cabeçalho
     return (
         <>
@@ -25,7 +32,19 @@ function Header() {
                 <span className="menu-links">
                     <a href="/about">ABOUT US</a>
                     <a href="/log-in">LOG IN</a>
-                </span>
+                </span>    
+                <button className="menu-drop" onClick={toggleAberto}>
+                    <CiMenuFries/> 
+                </button>
+                {estaAberto && (
+                    <div className="menu-tabela">
+                        <a href="/home">HOME</a>
+                        <a href="/events">EVENTS</a>
+                        <a href="/log-in">LOCATIONS</a>
+                        <a href="/about">ABOUT US</a>
+                        <a href="/log-in">LOG IN</a>
+                    </div>
+                )};
             </div>
 
             {/* Linha horizontal separadora */}
